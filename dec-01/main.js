d91bdab2f1a45a836551fd21d3f7a13f124d25c7
TJ <tttomdemaria@gmail.com>
2024-12-01 22:25:53 -0500

    Part one complete

--- dec-01/main.js
+++ dec-01/main.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const input = fs.readFileSync(`../puzzle-input/dec-01.txt`, "utf8", (err, data) => {
+const file = fs.readFileSync(`../puzzle-input/dec-01.txt`, "utf8", (err, data) => {
   if (err) {
     console.log(err);
   } else {
@@ -8,6 +8,31 @@ const input = fs.readFileSync(`../puzzle-input/dec-01.txt`, "utf8", (err, data) 
   }
 });
 
-const puzzleData = input.split("\n").forEach(el => {
-  return el.split(" ");
+let arrOne = [];
+let arrTwo = [];
+let accumulator = 0;
+
+const puzzleData = file.split("\n").map(e => e.replace("\r", ""));
+
+puzzleData.forEach(e => {
+  const tempArr = e.split("  ");
+  console.log(e);
+  arrOne.push(tempArr[0]);
+  arrTwo.push(tempArr[1]);
 });
+
+arrOne.sort();
+arrTwo.sort();
+
+arrOne.forEach((e, idx) => {
+  // console.log(Number(arrOne[idx]), "  ", Number(arrTwo[idx]));
+  accumulator += orderNums(Number(arrOne[idx]), Number(arrTwo[idx]));
+});
+
+function orderNums(input1, input2) {
+  // console.log(input1, " ", input2);
+  console.log(Math.max(input1, input2) - Math.min(input1, input2));
+  return Math.max(input1, input2) - Math.min(input1, input2);
+}
+
+console.log(accumulator);
